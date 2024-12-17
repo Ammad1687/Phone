8af3e5a32b756c991ffee22c72f5e804bea1c844
Ammad1687 <42167952+Ammad1687@users.noreply.github.com>
2024-12-17 18:43:55 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Function to display the welcome modal
+window.onload = function () {
+    document.getElementById("welcomeModal").style.display = "block";
+};
+
+// Function to close the modal
+function closeModal() {
+    document.getElementById("welcomeModal").style.display = "none";
+}
